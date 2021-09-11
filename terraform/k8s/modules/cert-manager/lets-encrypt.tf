754418b581e3582ceacc6d974420401cd2f90be5
Grant Birkinbine <grantbirki@github.com>
2021-09-11 00:00:19 -0700

    make lets-encrypt.tf wait for cert_manager_manifest

--- terraform/k8s/modules/cert-manager/lets-encrypt.tf
+++ terraform/k8s/modules/cert-manager/lets-encrypt.tf
@@ -4,7 +4,8 @@ data "kubectl_file_documents" "lets_encrypt_manifests" {
 
 resource "kubectl_manifest" "lets_encrypt_manifest" {
   depends_on = [
-    kubectl_manifest.cert_manager_namespace
+    kubectl_manifest.cert_manager_namespace,
+    kubectl_manifest.cert_manager_manifest
   ]
   count     = length(data.kubectl_file_documents.lets_encrypt_manifests.documents)
   yaml_body = element(data.kubectl_file_documents.lets_encrypt_manifests.documents, count.index)
