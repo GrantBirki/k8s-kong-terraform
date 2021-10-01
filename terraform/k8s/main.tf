74aedf220f1878b5e6973416b06609792f353490
Grant Birkinbine <grantbirki@github.com>
2021-09-30 19:51:32 -0700

    update all the k8s manifest files

--- terraform/k8s/main.tf
+++ terraform/k8s/main.tf
@@ -12,4 +12,10 @@ module "monitoring" {
 
 module "nginx_example" {
   source = "./modules/containers/nginx_example"
+  # Environment variables
+  IMAGE_TAG   = var.NGINX_IMAGE_TAG
+  ENVIRONMENT = var.ENVIRONMENT
+
+  # Config
+  ACR_NAME = data.azurerm_container_registry.acr.name
 }
